00068d5d2ff7553c61d10636c48b09e25a20df48
Lisa-marie-code <lisamariekoomson340@gmail.com>
2023-07-25 14:16:04 +0000

    header done

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,12 +1,26 @@
 import React from "react";
-import {Logo} from "./logo";
-import dev from '../../images/logo.png'
+import { Logo } from "./logo";
+import { BiLink } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import "./header.css";
+import dev from "../../images/logo.png";
 
 export const Header = () => {
   return (
     <div>
       <header className="dev-link">
-       <Logo img ={dev} />
+        <Logo img={dev} className="logo"/>
+        <div id="profile">
+          <button className="link">
+            <BiLink className="file-icon" />
+            Links
+          </button>
+          <button className="profile-details">
+            <CgProfile className="name-icon" />
+            Profile Details
+          </button>
+        </div>
+        <button className="preview">Preview</button>
       </header>
     </div>
   );
