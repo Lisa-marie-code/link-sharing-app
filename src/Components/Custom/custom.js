e2399ee56b6cd66a2d22b6b77873032035ec094d
Lisa-marie-code <lisamariekoomson340@gmail.com>
2023-07-25 16:05:45 +0000

    link area added

--- src/Components/Custom/custom.js
+++ src/Components/Custom/custom.js
@@ -1,11 +1,16 @@
 import React from "react";
-import './custom.css';
+import { Linknew } from "../Linknew/linknew";
+import "./custom.css";
 
 export const Custom = () => {
   return (
     <div className="custom_div">
       <h1>Customize your links</h1>
-      <p>Add/edit/remove links below and then share all your profiles with the world!</p>
+      <p>
+        Add/edit/remove links below and then share all your profiles with the
+        world!
+      </p>
+      <Linknew />
     </div>
   );
 };
---
+++ src/Components/Linknew/linknew.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+
+export const Linknew = () => {
+  return (
+    <div>
+      <button>
+        <AiOutlinePlus />
+        Add new link
+      </button>
+    </div>
+  );
+};
+
+export default Linknew;
