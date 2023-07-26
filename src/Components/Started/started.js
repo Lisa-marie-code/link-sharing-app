bcbfddee13f5e3da23e7835c2ff5b4d1dd8c6b21
Lisa-marie-code <lisamariekoomson340@gmail.com>
2023-07-26 14:02:38 +0000

    started session

--- src/Components/Started/started.js
+++ src/Components/Started/started.js
@@ -1,11 +1,20 @@
 import React from "react";
+import "./started.css";
 import { Point } from "../Started/pointhand";
 import hand from "../../images/humanPoint.svg";
 
 export const Started = () => {
   return (
-    <div>
-      <Point img={hand} />
+    <div >
+      <div className="started-text">
+        <Point img={hand} />
+        <h2>Let's get you started</h2>
+        <p id="description">
+          Use the "Add new link" button to get started.Once you have more than
+          one link you can reorder and edit them.We're here to help you share
+          your profiles with everyone!
+        </p>
+      </div>
     </div>
   );
 };
