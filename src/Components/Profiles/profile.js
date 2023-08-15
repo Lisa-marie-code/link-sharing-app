eacd4723e4438330bca8a5cec14e7b8fc6a25165
Lisa-marie-code <lisamariekoomson340@gmail.com>
2023-08-15 14:00:27 +0000

    added form to profile

--- src/Components/Profiles/profile.js
+++ src/Components/Profiles/profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./profile.css"
+import "./profile.css";
 import { AiOutlinePicture, AiOutlinePlus } from "react-icons/ai";
 
 export const Profile = () => {
@@ -8,12 +8,29 @@ export const Profile = () => {
       <h1>Profile Details</h1>
       <p>Add your details to create a personal touch to your profile.</p>
       <div className="upload_pic">
-        <p>Profile picture</p>
+        <p className="profile-text">Profile picture</p>
         <div className="image">
-          <AiOutlinePicture />
-          <AiOutlinePlus />Upload Image
+          <AiOutlinePicture className="picture" />
+          <AiOutlinePlus />
+          Upload Image
         </div>
-        <p>Image must be below 1024x1024px. Use PNG or JPG format.</p>
+        <p className="caution">
+          Image must be below 1024x1024px. Use PNG or JPG format.
+        </p>
+      </div>
+      <div className="formCover">
+        <div className="form-group">
+          <label for="first-name">First Name*</label>
+          <input id="first-name" name="first-name" placeholder="e.g. John" />
+        </div>
+        <div className="form-group">
+          <label for="first-name">Last Name*</label>
+          <input id="first-name" name="first-name" placeholder="e.g. Appleseed" />
+        </div>
+        <div className="form-group">
+          <label for="first-name">Email</label>
+          <input id="first-name" name="first-name" placeholder="e.g. email@example.com" />
+        </div>
       </div>
     </div>
   );
